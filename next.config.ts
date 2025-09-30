ab7e3642f95fe6f2b60d21af79982dccc98329cc
cimisachimi <mfaturrahmann@gmail.com>
2025-09-30 09:53:38 +0700

    fix the stuff

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,24 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8000",
-        pathname: "/storage/**",
-      },
-      {
-        protocol: "https",
-        hostname: "api.indocharcoalsupply.com",
-        pathname: "/storage/**",
-      },
-    ],
-  },
+const withNextIntl = createNextIntlPlugin();
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: '/:path*',
         headers: [
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.google.com https://*.googleapis.com https://*.google-analytics.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://*.google.com https://*.googleapis.com https://*.google-analytics.com;
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               img-src 'self' data: https://api.indocharcoalsupply.com http://localhost:8000 https://*.google-analytics.com;
               font-src 'self' https://fonts.gstatic.com;
-              connect-src 'self' https://api.indocharcoalsupply.com https://*.google.com https://*.googleapis.com https://*.google-analytics.com https://maps.googleapis.com https://maps.gstatic.com;
-            `.replace(/\s{2,}/g, " ").trim(),
+              connect-src 'self' https://api.indocharcoalsupply.com http://localhost:3000 http://127.0.0.1:3000 https://*.google.com https://*.googleapis.com https://*.google-analytics.com https://maps.googleapis.com https://maps.gstatic.com;
+            `.replace(/\s{2,}/g, ' ').trim(),
           },
         ],
       },
@@ -39,4 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
